7da88175cc63d8587694fd5e91e839470ee1e9f3
Usman Suleiman <usmansbk@gmail.com>
2021-08-02 10:29:16 +0100

    Hook listeners

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -13,18 +13,29 @@ const displayItems = ({ meals = [] }) => {
                 <div class="card-header">
                     <h4>${name}</h4>
                     <div class="like-button">
-                        <button>
+                        <button data-id="like-meal">
                             <span class="material-icons">favorite_border</span>
                         </button>
                         <p><span data-id="likes">0</span> Likes</p>
                     </div>
                 </div>
                 <div class="card-buttons">
-                    <button class="primary-button">Comments</button>
-                    <button class="primary-button">Reservations</button>
+                    <button data-id="comments" class="primary-button">Comments</button>
+                    <button data-id="reservations" class="primary-button">Reservations</button>
                 </div>
             </div>
         `;
+
+    const commentsButton = card.querySelector('[data-id="comments"]');
+    const likeButton = card.querySelector('[data-id="like-meal"]');
+
+    commentsButton.addEventListener('click', () => {
+      console.log(id);
+    });
+
+    likeButton.addEventListener('click', () => {
+      console.log('Like', id);
+    });
 
     grid.appendChild(card);
   });
