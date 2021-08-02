2056429a7f52094e47283c86fd0ef5ce4f32e47b
Usman Suleiman <usmansbk@gmail.com>
2021-08-02 10:39:37 +0100

    Fetch likes involvement

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,20 @@ import displayHomePage from './homepage.js';
 import * as API from './api.js';
 
 export default async () => {
+  const appId = 'fakeId' || await API.createApp();
   const meals = await API.fetchMeals();
+  const likes = [] || await API.fetchLikes(appId);
+
   const formattedMeals = meals.map(({ idMeal: id, strMeal: name, strMealThumb: src }) => ({
     id,
     name,
     src,
   }));
+
+  likes.forEach((involvement) => {
+    const meal = formattedMeals[involvement.item_id];
+    meal.likes = involvement.likes;
+  });
 
   displayHomePage({ meals: formattedMeals });
 };
