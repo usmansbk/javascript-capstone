b8cc16dbc26cbcfef23c666607aca61ee5e657d8
Usman Suleiman <usmansbk@gmail.com>
2021-08-03 10:55:18 +0100

    Disable webpack devtool

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       },
     ],
   },
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
