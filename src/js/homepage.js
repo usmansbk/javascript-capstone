60ed9008267932039d0308bc9066413dfb4075bf
Usman Suleiman <usmansbk@gmail.com>
2021-08-03 17:19:04 +0100

    Fix spacing

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -40,23 +40,23 @@ const displayItems = ({ meals = [], likeMeal, onPressCommentsButton }) => {
     const card = document.createElement('div');
     card.classList.add('card');
     card.innerHTML = `
-            <img src=${src} alt="" class="card-image" />
-            <div class="card-body">
-                <div class="card-header">
-                    <h4>${name}</h4>
-                    <div class="like-button">
-                        <button data-id="like-meal">
-                            <span class="material-icons">favorite_border</span>
-                        </button>
-                        <p><span data-id="likes">${likes}</span> Likes</p>
-                    </div>
-                </div>
-                <div class="card-buttons">
-                    <button data-id="comments" class="primary-button">Comments</button>
-                    <button data-id="reservations" class="primary-button">Reservations</button>
-                </div>
-            </div>
-        `;
+      <img src=${src} alt="" class="card-image" />
+      <div class="card-body">
+          <div class="card-header">
+              <h4>${name}</h4>
+              <div class="like-button">
+                  <button data-id="like-meal">
+                      <span class="material-icons">favorite_border</span>
+                  </button>
+                  <p><span data-id="likes">${likes}</span> Likes</p>
+              </div>
+          </div>
+          <div class="card-buttons">
+              <button data-id="comments" class="primary-button">Comments</button>
+              <button data-id="reservations" class="primary-button">Reservations</button>
+          </div>
+      </div>
+    `;
 
     const commentsButton = card.querySelector('[data-id="comments"]');
     const likeButton = card.querySelector('[data-id="like-meal"]');
